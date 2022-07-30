fd6ddb40cb0aeaa5f18293792f945bfccee53ad2
Ivan <ivanyu327@gmail.com>
2022-07-30 16:37:36 -0400

    predictions and validation testing

--- medicationSideEffect.py
+++ medicationSideEffect.py
@@ -12,6 +12,14 @@ from tensorflow.keras.layers import Activation,Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+import os.path
+
+from tensorflow.keras.models import load_model
+
 # Example data:
 # An experimental drug was tested on individuals from ages 13 to 100 in a clinical trial.
 # The trial had 2100 participants.Half were under 65 years old,half were 65 years or older.
@@ -66,4 +74,97 @@ model.summary()
 
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(x=scaled_train_samples, y=train_labels, validation_split = 0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
+
+
+
+
+
+
+test_labels = []
+test_samples = []
+
+for i in range(10):
+    # The ~5% of younger individuals who did experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+
+    # The ~5% of older individuals who did not experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+for i in range(200):
+    # The ~95% of younger individuals who did not experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+
+    # The ~95% of oLder individuals who did experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+
+test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
+test_labels,test_samples = shuffle(test_labels,test_samples)
+
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1,1))
+
+predictions = model.predict(x=scaled_test_samples,batch_size=10,verbose=0)
+
+# for i in predictions:
+#     print(i)
+
+rounded_predictions = np.argmax(predictions,axis=-1)
+
+# for i in rounded_predictions:
+#     print(i)
+
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+def plot_confusion_matrix(cm,classes,normalize=False,title='Confusion matrix',cmap=plt.cm.Blues):
+    # This function prints and plots the confusion matrix.
+    # Normalization can be applied by setting normalize=True'.
+    
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks,classes,rotation=45)
+    plt.yticks(tick_marks,classes)
+
+    if normalize:
+        cm = cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without seetion')
+
+    print(cm)
+    thresh = cm.max()/2.
+    for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i,cm[i,j],
+            horizontalalignment="center",
+            color="white"if cm[i,j]>thresh else "black")
+    
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+cm_plot_labels =['no_side_effects','had_side_effects']
+plot_confusion_matrix(cm=cm,classes=cm_plot_labels,title='Confusion Matrix')
+
+
+
+if os.path.isfile('models/medical_trial model.h5') is False:
+    model.save('models/medical_trial_model.h5')
+
+
+
+new_model = load_model('models/medical_trial_model.h5')
+
+new_model.summary()
+new_model.get_weights()
+new_model.optimizer
